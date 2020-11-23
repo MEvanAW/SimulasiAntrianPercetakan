33b72005dc3b295ff6f4bdffe8d28477ad6b68f6
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-23 19:26:14 +0700

    Menyederhanakan Antrian

--- SimulasiAntrianPercetakan/Percetakan.cs
+++ SimulasiAntrianPercetakan/Percetakan.cs
@@ -9,7 +9,8 @@ namespace SimulasiAntrianPercetakan
     static class Percetakan
     {
         // Atribut percetakan
-        public static LinkedList<Pesanan> Antrian = new LinkedList<Pesanan>();
+        private static Queue<Pesanan> _antrianBiasa = new Queue<Pesanan>();
+        private static Queue<Pesanan> _antrianEkspres = new Queue<Pesanan>();
         public static List<Pesanan> BerkasTercetak = new List<Pesanan>();
         private static int _jumlahPrinter = 1;
         private static int _jumlahPrinterOn = 1;
@@ -24,6 +25,13 @@ namespace SimulasiAntrianPercetakan
                     // Maka dilakukan penyesuaian:
                     _jumlahPrinterOn = _jumlahPrinter; }
         }
+        public static List<Pesanan> AntrianBiasa {
+            get { return _antrianBiasa.ToList(); }
+        }
+        public static List<Pesanan> AntrianEkspres
+        {
+            get { return _antrianEkspres.ToList(); }
+        }
         public static int JumlahPrinterOn{
             get { return _jumlahPrinterOn; }
             set {
@@ -33,50 +41,27 @@ namespace SimulasiAntrianPercetakan
                      // Maka dilakukan penyesuaian:
                     _jumlahPrinterOn = _jumlahPrinter; }
         }
-        public static List<Pesanan> AntrianBiasa{
-            get {
-                List<Pesanan> returnList = new List<Pesanan>();
-                foreach (Pesanan pesanan in Antrian)
-                    if(!pesanan.isEkspres)
-                        returnList.Add(pesanan);
-                return returnList;
-            }
-        }
-        public static List<Pesanan> AntrianEkspres{
-            get {
-                List<Pesanan> returnList = new List<Pesanan>();
-                foreach (Pesanan pesanan in Antrian)
-                    if (pesanan.isEkspres)
-                        returnList.Add(pesanan);
-                return returnList;
-            }
-        }
 
         // Behaviour percetakan
         public static void TerimaPesanan(Pesanan pesanan)
         {
-            var nodePesanan = new LinkedListNode<Pesanan>(pesanan);
-            if (Antrian.First == null)
-                Antrian.AddFirst(nodePesanan);
+            if (!pesanan.isEkspres)
+                _antrianBiasa.Enqueue(pesanan);
             else
-            {
-                var pointer = Antrian.First;
-                while (pointer.Next != null && pointer.Value.isEkspres && !pesanan.isEkspres)
-                    pointer = pointer.Next;
-                while (pointer.Next != null && pointer.Value.timeStamp < pesanan.timeStamp && pointer.Value.isEkspres == pesanan.isEkspres)
-                    pointer = pointer.Next;
-                if (pointer.Value.timeStamp <= pesanan.timeStamp)
-                    Antrian.AddAfter(pointer, pesanan);
-                else
-                    Antrian.AddBefore(pointer, pesanan);
-            }
+                _antrianEkspres.Enqueue(pesanan);
         }
         public static bool Cetak()
         {
-            if (Antrian.Any())
+            if (_antrianEkspres.Any())
             {
-                BerkasTercetak.Add(Antrian.First.Value);
-                Antrian.RemoveFirst();
+                BerkasTercetak.Add(_antrianEkspres.Peek());
+                _antrianEkspres.Dequeue();
+                return true;
+            }
+            else if (_antrianBiasa.Any())
+            {
+                BerkasTercetak.Add(_antrianBiasa.Peek());
+                _antrianBiasa.Dequeue();
                 return true;
             }
             else
